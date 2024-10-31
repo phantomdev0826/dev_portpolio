e88db365f83074a852b58c1564a1f647b6367acf
Shinn Thant <94442302+DevShinnThant@users.noreply.github.com>
2024-10-31 09:42:50 +0630

    refactor: change resume link

--- components/ResumeBtn.tsx
+++ components/ResumeBtn.tsx
@@ -69,7 +69,7 @@ export default function ResumeBtn() {
   return (
     <a
       role="button"
-      href="https://shin-thant-portfolio.tiiny.site/"
+      href="https://crimson-wendeline-56.tiiny.site/"
       target="_blank"
       rel="noopener noreferrer"
       aria-label="Resume"
